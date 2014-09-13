96646de1d1f50d095934ff6f645b70313ca2ccb8
Kenny L. Tran <unique@kennyletran.com>
2014-09-13 16:15:16 -0700

    beginning

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Topofbarrel::Application.config.secret_token = 'a62c1d136394ddf07161138431439a217f84b6ad5a5cbaa35a858e1af02402df4e72ea53fe387362c5a52cb23185dbef4eace22339edda7407df4f272581e404'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Topofbarrel::Application.config.session_store :cookie_store, key: '_topofbarrel_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Topofbarrel::Application.config.session_store :active_record_store
