6c8cd3d21f19fc563137898499fe6be626a7a4a2
Kenny L. Tran <unique@kennyletran.com>
2014-09-14 15:54:05 -0700

    added genres to movie info

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,11 @@ class MoviesController < ApplicationController
 			# rand_id = rand(Movie.count)
  		# 	@movie = Movie.first(:conditions => [ "id >= ?", rand_id])
  			@movie = Movie.order("RANDOM()").limit(1)[0]
+ 			# @movie = Movie.order("RAND()").limit(1)[0] # for sqlite
  		end
  		@audience_class = @movie.audience_rating > 60 ? "fresh_popcorn" : "spilled_popcorn"
  		movie_id = @movie.rotten_tomatoes_id
  		@rt_link = "http://www.rottentomatoes.com/m/#{movie_id}"
+ 		@genres = @movie.genres.split(',').join(', ')
 	end
 end
