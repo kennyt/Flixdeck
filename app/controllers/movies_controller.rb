4e91b4f2ab5a1694b760b17050212cf9452d8679
Kenny L. Tran <unique@kennyletran.com>
2014-09-14 15:39:28 -0700

    changing random query

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,8 +5,9 @@ class MoviesController < ApplicationController
 	def show
 		@movie = false
 		until !@movie == false && @movie.critic_rating > 59
-			rand_id = rand(Movie.count)
- 			@movie = Movie.first(:conditions => [ "id == ?", rand_id])
+			# rand_id = rand(Movie.count)
+ 		# 	@movie = Movie.first(:conditions => [ "id >= ?", rand_id])
+ 			@movie = Movie.order("RANDOM()").limit(1)
  		end
  		@audience_class = @movie.audience_rating > 60 ? "fresh_popcorn" : "spilled_popcorn"
  		movie_id = @movie.rotten_tomatoes_id
