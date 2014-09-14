1ec3da8d403f3aba5f25eccd319257d49c59ffea
Kenny L. Tran <unique@kennyletran.com>
2014-09-14 16:19:33 -0700

    changed query again

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,15 +4,25 @@ class MoviesController < ApplicationController
 
 	def show
 		@movie = false
-		until !@movie == false && @movie.critic_rating > 59
+		until pass_filter?(@movie)
 			# rand_id = rand(Movie.count)
  		# 	@movie = Movie.first(:conditions => [ "id >= ?", rand_id])
- 			@movie = Movie.order("RANDOM()").limit(1)[0]
+ 			@movie = Movie.where('critic_rating > 59').order("RANDOM()").limit(1)
+ 			# movies.each do |movie|
+ 			# 	if pass_filter?(movie)
+ 			# 		@movie = movie
+ 			# 		break
+ 			# 	end
+ 			# end
  			# @movie = Movie.order("RAND()").limit(1)[0] # for sqlite
  		end
  		@audience_class = @movie.audience_rating > 60 ? "fresh_popcorn" : "spilled_popcorn"
  		movie_id = @movie.rotten_tomatoes_id
  		@rt_link = "http://www.rottentomatoes.com/m/#{movie_id}"
  		@genres = @movie.genres.split(',').join(', ')
+	end
+
+	def pass_filter?(movie)
+		movie != false && movie.critic_rating > 59
 	end
 end
