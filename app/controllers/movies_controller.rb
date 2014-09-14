367f6ff4a7490ed780aef6ef0fcddd04179ebc3e
Kenny L. Tran <unique@kennyletran.com>
2014-09-14 15:22:31 -0700

    rearranged design

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,5 +9,7 @@ class MoviesController < ApplicationController
  			@movie = Movie.first(:conditions => [ "id >= ?", rand_id])
  		end
  		@audience_class = @movie.audience_rating > 60 ? "fresh_popcorn" : "spilled_popcorn"
+ 		movie_id = @movie.rotten_tomatoes_id
+ 		@rt_link = "http://www.rottentomatoes.com/m/#{movie_id}"
 	end
 end
