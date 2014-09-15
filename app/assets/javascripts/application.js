ebde826b6751194927ff74b9c1e23700976331ae
Kenny L. Tran <unique@kennyletran.com>
2014-09-14 22:25:48 -0700

    added open bar for synopsis' that are too long

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,34 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+function checkIfOpenBarNeeded(synopsis){
+	var height = synopsis.height()
+	var autoHeight = synopsis.css('height','auto').height();
+	synopsis.height(height)
+
+	if (autoHeight >= height + 10){
+		synopsis.addClass('unopened')
+		synopsis.append('<div class="open_bar">v</div>')
+	}
+}
+
+$(document).ready(function(){
+	$('body').on('click', '.synopsis', function(ev){
+		if ($(this).hasClass('unopened')){
+			var that = this;
+			var height = $(this).height()
+			var autoHeight = $(this).css('height','auto').height();
+			var animationTime = 400
+
+			$(that).removeClass('unopened')
+			$(this).height(height).animate({height: autoHeight}, animationTime);
+			$('.open_bar').fadeOut(animationTime)
+			setTimeout(function(){
+				$('.open_bar').remove()
+			},animationTime)
+		}
+	})
+
+	checkIfOpenBarNeeded($('.synopsis'));
+})
